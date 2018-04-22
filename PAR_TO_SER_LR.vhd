f0f9ce059d550ea9697678011abb81e3d4b0b585
Ian F <ianmichael.fennie@haw-hamburg.de>
2018-04-22 14:16:44 +0200

    Add sequential process for shift registers in PAR_TO_SER

--- PAR_TO_SER_LR.vhd
+++ PAR_TO_SER_LR.vhd
@@ -17,10 +17,24 @@ end PAR_TO_SER_LR;
 
 architecture BEHAVIORAL of PAR_TO_SER_LR is
 
-signal PAR_REG_L_NEXT, PAR_REG_L : std_logic_vector( W-1 downto 0);
-signal PAR_REG_R_NEXT, PAR_REG_R : std_logic_vector( W-1 downto 0);
+signal SHIFT_REG_L_NEXT, SHIFT_REG_L : std_logic_vector( W-1 downto 0);
+signal SHIFT_REG_R_NEXT, SHIFT_REG_R : std_logic_vector( W-1 downto 0);
 
 begin
+
+-- sequential process for shift registers
+P2S_REGS: process(CLK)
+begin
+	if CLK = '1' and CLK'event then
+		if SRESETN = '0' then
+			SHIFT_REG_L <= (others=>'0') after 2 ns;
+			SHIFT_REG_R <= (others=>'0') after 2 ns;
+		else 
+			SHIFT_REG_L <= SHIFT_REG_L_NEXT after 2 ns;
+			SHIFT_REG_R <= SHIFT_REG_R_NEXT after 2 ns;
+		end if;
+	end if;
+end process;
 
 
 
